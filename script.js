ded5ae15836408ae820db6b508e0d1095189703c
GiverPlay007 <play.giver.007@gmail.com>
2021-02-04 19:42:14 -0300

    Corrigir a formatação do amount

--- script.js
+++ script.js
@@ -113,8 +113,8 @@ const DOM = {
 
 const Utils = {
   formatValue(value) {
-    value = Number(value.replace(/\.\,/g)) * 100;
-    return value;
+    value = value * 100;
+    return Math.round(value);
   },
 
   formatCurrency(value) {
